c457f94f3e0a8de318bc483054250076b7df0ec2
Vishnu Anand <vishnuanand2000@gmail.com>
2022-09-05 15:40:17 +0530

    Added create product rest method

--- product-service/src/main/java/com/vishnu/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/vishnu/productservice/controller/ProductController.java
@@ -1,12 +1,17 @@
 package com.vishnu.productservice.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.vishnu.productservice.ddo.ProductRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
 
+    }
 
 }
